4540f5afd0fd39aad1ef4b6905c1ab4fe8f4cf6e
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-11-22 22:16:56 +0900

    Initial Commit

---
+++ CharacterAlarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CharacterAlarm
+//
+//  Created by junpei ono on 2019/11/22.
+//  Copyright © 2019 com.swiswiswift.swiftui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
