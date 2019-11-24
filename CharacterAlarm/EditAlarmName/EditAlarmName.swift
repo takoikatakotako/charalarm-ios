d8d66bb66da182936a5cfc82d52d924b2eccc23d
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-11-24 23:38:24 +0900

    アラームを設定

---
+++ CharacterAlarm/EditAlarmName/EditAlarmName.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct EditAlarmName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditAlarmName_Previews: PreviewProvider {
+    static var previews: some View {
+        EditAlarmName()
+    }
+}
