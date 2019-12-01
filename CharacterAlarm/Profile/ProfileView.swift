7e16c078d762a3f7dec26609ef525037fce7f440
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-12-01 23:13:02 +0900

    レイアウトを調整した。

--- CharacterAlarm/Profile/ProfileView.swift
+++ CharacterAlarm/Profile/ProfileView.swift
@@ -13,22 +13,49 @@ struct ProfileView: View {
             Section(header: ProfileIcon()) {
                 VStack(alignment: .leading) {
                     Text("名前")
+                        .font(Font.headline)
+                        .foregroundColor(Color.gray)
+                        .padding(.top, 8)
+                        .padding(.bottom, 8)
                     Text("井上結衣")
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 8)
+
                 }
 
                 VStack(alignment: .leading) {
                     Text("プロフィール")
+                        .font(Font.headline)
+                        .foregroundColor(Color.gray)
+                        .padding(.top, 8)
+                        .padding(.bottom, 8)
                     Text("色白で水色の髪と、赤い瞳を持つ細身の少女。エヴァンゲリオン零号機のパイロット（1人目の適格者＝ファーストチルドレン）。EVA零号機の起動実験の事故により重傷を負い、初登場時は包帯姿で登場する。過去の経歴は全て抹消済みであり、本作における最大のキーパーソンとして重大な役割を果たす。")
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 8)
+
                 }
 
                 VStack(alignment: .leading) {
                     Text("サークル名")
+                        .font(Font.headline)
+                        .foregroundColor(Color.gray)
+                        .padding(.top, 8)
+                        .padding(.bottom, 8)
                     Text("旋風鬼")
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 8)
+
                 }
 
                 VStack(alignment: .leading) {
                     Text("CV")
+                        .font(Font.headline)
+                        .foregroundColor(Color.gray)
+                        .padding(.top, 8)
+                        .padding(.bottom, 8)
                     Text("Mai")
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom, 8)
                 }
             }
         }
