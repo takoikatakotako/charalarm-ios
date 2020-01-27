341d05bb23017543207a76eab4a45819235d8fbf
snorlax.chemist.and.jazz@gmail.com <snorlax.chemist.and.jazz@gmail.com>
2020-01-28 01:12:56 +0900

    トップのデザインを整えた。

--- CharacterAlarm/ContentView.swift
+++ CharacterAlarm/ContentView.swift
@@ -4,35 +4,47 @@ struct ContentView: View {
     @State private var showNews: Bool = false
     @State private var showConfig: Bool = false
     var body: some View {
-        ZStack {
-            Image("background")
-                .resizable()
-                .scaledToFill()
-
-            VStack {
-                Image("normal")
+        GeometryReader { geometory in
+            ZStack {
+                Image("background")
                     .resizable()
-                    .padding(.top, 140.0)
-            }
-
-            VStack {
-                Button(action: {
-                    self.showNews = true
-                }) {
-                    Image("top-news")
-                }.sheet(isPresented: self.$showNews) {
-                    NewsView()
-                }.frame(width: 80, height: 80, alignment: .bottomLeading)
-                    .background(Color.red)
-
-                Button(action: {
-                    self.showConfig = true
-                }) {
-                    Image("top-config")
-                }.sheet(isPresented: self.$showConfig) {
-                    ConfigView()
-                }.frame(width: 80, height: 80, alignment: .bottomLeading)
-                .background(Color.red)
+                    .scaledToFill()
+                
+                VStack {
+                    Spacer()
+                    Image("normal")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometory.size.width, height: geometory.size.height - 100)
+                }
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            self.showNews = true
+                        }) {
+                            Image("top-news")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        }.sheet(isPresented: self.$showNews) {
+                            NewsView()
+                        }.frame(width: 60, height: 60, alignment: .bottomLeading)
+                            .background(Color.red)
+                        
+                        Button(action: {
+                            self.showConfig = true
+                        }) {
+                            Image("top-config")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        }.sheet(isPresented: self.$showConfig) {
+                            ConfigView()
+                        }.frame(width: 60, height: 60, alignment: .bottomLeading)
+                            .background(Color.red)
+                    }.padding(24)
+                }
             }
         }.edgesIgnoringSafeArea([.top, .bottom])
     }
