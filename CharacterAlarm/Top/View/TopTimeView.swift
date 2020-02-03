bf66e2fac072e517e09385e4db80c49d0ff35ff2
snorlax.chemist.and.jazz@gmail.com <snorlax.chemist.and.jazz@gmail.com>
2020-02-04 01:57:31 +0900

    日付のところやろう

--- CharacterAlarm/Top/View/TopTimeView.swift
+++ CharacterAlarm/Top/View/TopTimeView.swift
@@ -1,6 +1,19 @@
 import SwiftUI
 
 struct TopTimeView: View {
+    @State private var hour: Int = 0
+//    @State var minute: Int = 0
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    init() {
+        let date = Date()
+        let calendar = Calendar.current
+
+//        self.hour = 0
+        // hour = calendar.component(.hour, from: date)
+//        minute = calendar.component(.minute, from: date)
+    }
+
     var body: some View {
         VStack {
             Text("01:29")
@@ -8,18 +21,22 @@ struct TopTimeView: View {
                 .font(Font.system(size: 40))
             Text("01/29(W)")
                 .foregroundColor(.white)
-            .font(Font.system(size: 28))
+                .font(Font.system(size: 28))
         }
         .frame(width: 160, height: 160)
         .background(Color.black)
         .cornerRadius(80)
         .opacity(0.9)
+        .onReceive(timer) { _ in
+//            self.currentDate = input
+//            print(self.currentDate)
+        }
     }
 }
 
 struct TopTimeView_Previews: PreviewProvider {
     static var previews: some View {
         TopTimeView()
-        .previewLayout(.fixed(width: 160, height: 160))
+            .previewLayout(.fixed(width: 160, height: 160))
     }
 }
