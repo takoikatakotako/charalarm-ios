f39e7e6f73bd06d3a366f93ad31ac8727f108a92
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2020-02-06 13:01:23 +0900

    グリッド表示にした。

--- CharacterAlarm/CharacterList/CharacterListView.swift
+++ CharacterAlarm/CharacterList/CharacterListView.swift
@@ -1,31 +1,16 @@
 import SwiftUI
 import UIKit
 import SDWebImageSwiftUI
+import QGrid
 
 struct CharacterListView: View {
     @ObservedObject(initialValue: CharacterListViewModel()) var viewModel: CharacterListViewModel
+
     var body: some View {
         ZStack {
-            List(viewModel.profiles) { profile in
-                NavigationLink(destination: ProfileView(profile: profile)) {
-                    HStack {
-                        WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic"))
-                            .resizable()
-                            .placeholder {
-                                Image("profile")
-                                    .resizable()
-                                    .frame(width: 54, height: 54)
-                        }
-                            .animation(.easeInOut(duration: 0.5)) // Animation Duration
-                            .transition(.fade) // Fade Transition
-                            .scaledToFit()
-                            .frame(width: 54.0, height: 54, alignment: .center)
-
-                        Text(profile.name)
-                    }.frame(height: 60)
-                }
-            }.listStyle(DefaultListStyle())
-
+            QGrid(viewModel.profiles, columns: 3) { profile in
+                GridCell(profile: profile)
+            }
             VStack {
                 Spacer()
                 VStack {
@@ -34,14 +19,39 @@ struct CharacterListView: View {
                         Spacer()
                     }
                     Text("キャラクターを追加しませんか？？")
-
                 }
                 .background(Color.white)
-            .border(Color.red, width: 2)
+                .border(Color.red, width: 2)
             }.padding()
         }
     }
 }
+
+struct GridCell: View {
+    let profile: Profile
+    var body: some View {
+        ZStack {
+            Image("profile")
+                .resizable()
+                .frame(width: 100, height: 160, alignment: .center)
+                .scaledToFit()
+
+            Text("person.firstName").lineLimit(1)
+        }
+    }
+}
+
+//WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic"))
+//    .resizable()
+//    .placeholder {
+//        Image("profile")
+//            .resizable()
+//            .frame(width: 54, height: 54)
+//}
+//    .animation(.easeInOut(duration: 0.5)) // Animation Duration
+//    .transition(.fade) // Fade Transition
+//    .scaledToFit()
+//    .frame(width: 54.0, height: 54, alignment: .center)
 
 struct CharacterList_Previews: PreviewProvider {
     static var previews: some View {
