69e779465a05c99d96a0acb6bfb4a27546e834d0
snorlax.chemist.and.jazz@gmail.com <snorlax.chemist.and.jazz@gmail.com>
2020-02-08 00:13:59 +0900

    グリッドビューを実装した。

--- CharacterAlarm/CharacterList/CharacterListView.swift
+++ CharacterAlarm/CharacterList/CharacterListView.swift
@@ -1,44 +1,85 @@
 import SwiftUI
 import UIKit
 import SDWebImageSwiftUI
-import QGrid
 
-struct CharacterListView: View {
-    @ObservedObject(initialValue: CharacterListViewModel()) var viewModel: CharacterListViewModel
+struct Pokemon: Identifiable {
+    let id = UUID()
+    let name: String
+    let imageName: String
+}
 
+struct PokemonView: View {
+    let pokemon: Pokemon
     var body: some View {
         ZStack {
-            QGrid(viewModel.profiles, columns: 3) { profile in
-                GridCell(profile: profile)
-            }
+            Image(pokemon.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
             VStack {
                 Spacer()
-                VStack {
-                    HStack {
-                        Text("PR")
-                        Spacer()
-                    }
-                    Text("キャラクターを追加しませんか？？")
-                }
-                .background(Color.white)
-                .border(Color.red, width: 2)
-            }.padding()
+                Text(pokemon.name)
+                    .font(Font.system(size: 20.0).bold())
+                    .frame(maxWidth: .infinity, maxHeight: 32)
+                    .background(Color.gray)
+                    .foregroundColor(Color.white)
+                    .opacity(0.8)
+            }
         }
     }
 }
 
-struct GridCell: View {
-    let profile: Profile
-    var body: some View {
-        ZStack {
-            Image("profile")
-                .resizable()
-                .frame(width: 100, height: 160, alignment: .center)
-                .scaledToFit()
+struct CharacterListView: View {
+    @ObservedObject(initialValue: CharacterListViewModel()) var viewModel: CharacterListViewModel
 
-            Text("person.firstName").lineLimit(1)
-        }
+    let columns: Int = 3
+    let pokemons = [
+        Pokemon(name: "カビゴン", imageName: "snorlax"),
+        Pokemon(name: "ゲンガー", imageName: "gengar"),
+        Pokemon(name: "ヌオー", imageName: "quagsire"),
+        Pokemon(name: "コイル", imageName: "magnemite"),
+        Pokemon(name: "ミニリュウ", imageName: "dratini"),
+        Pokemon(name: "ヤドン", imageName: "slowpoke"),
+        Pokemon(name: "コダック", imageName: "psyduck")
+    ]
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(showsIndicators: false) {
+                ForEach(0..<self.pokemons.count/self.columns) { rowIndex in
+                    HStack {
+                        ForEach(0..<self.columns) { columnIndex in
+                            PokemonView(pokemon: self.pokemons[self.columns * rowIndex + columnIndex])
+                                .frame(width: self.cellWidth(width: geometry.size.width),
+                                       height: self.cellHeight(width: geometry.size.width))
+                                .border(Color.black, width: 2)
+                                .clipped()
+                        }
+                    }
+                }
+
+                if (self.pokemons.count % self.columns > 0) {
+                    HStack {
+                        ForEach(0..<self.pokemons.count % self.columns) { column in
+                            PokemonView(pokemon: self.pokemons[self.columns * (self.pokemons.count / self.columns) + column])
+                                .frame(width: self.cellWidth(width: geometry.size.width),
+                                       height: self.cellHeight(width: geometry.size.width))
+                                .border(Color.black, width: 2)
+                                .clipped()
+                        }
+                        Spacer()
+                    }
+                }
+            }
+        }.padding()
     }
+
+    private func cellWidth(width: CGFloat) -> CGFloat {
+        return width / CGFloat(columns)
+    }
+
+    private func cellHeight(width: CGFloat) -> CGFloat {
+        return cellWidth(width: width) * 1.5
+    }
+
 }
 
 //WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic"))
