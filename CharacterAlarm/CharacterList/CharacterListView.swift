20afa6c3f09b033f2764af56736ca6220c550fae
snorlax.chemist.and.jazz@gmail.com <snorlax.chemist.and.jazz@gmail.com>
2020-02-08 11:13:44 +0900

    Profile を押すとそんな感じにする。

--- CharacterAlarm/CharacterList/CharacterListView.swift
+++ CharacterAlarm/CharacterList/CharacterListView.swift
@@ -33,30 +33,41 @@ struct CharacterListView: View {
                 ForEach(0..<self.viewModel.profiles.count/self.columns) { rowIndex in
                     HStack {
                         ForEach(0..<self.columns) { columnIndex in
-
-                            ProfileCell(profile: self.viewModel.profiles[self.columns * rowIndex + columnIndex])
-                                .frame(width: self.cellWidth(width: geometry.size.width),
-                                       height: self.cellHeight(width: geometry.size.width))
-                                .border(Color.black, width: 2)
-                                .clipped()
+                            NavigationLink(destination: ProfileView(profile: self.getProfile(rowIndex: rowIndex, columnIndex: columnIndex))) {
+                                ProfileCell(profile: self.getProfile(rowIndex: rowIndex, columnIndex: columnIndex))
+                                    .frame(width: self.cellWidth(width: geometry.size.width),
+                                           height: self.cellHeight(width: geometry.size.width))
+                                    .border(Color.black, width: 2)
+                                    .clipped()
+                            }
                         }
                     }
                 }
 
                 if (self.viewModel.profiles.count % self.columns > 0) {
                     HStack {
-                        ForEach(0..<self.viewModel.profiles.count % self.columns) { column in
-                            ProfileCell(profile: self.viewModel.profiles[self.columns * (self.viewModel.profiles.count / self.columns) + column])
-                                .frame(width: self.cellWidth(width: geometry.size.width),
-                                       height: self.cellHeight(width: geometry.size.width))
-                                .border(Color.black, width: 2)
-                                .clipped()
+                        ForEach(0..<self.viewModel.profiles.count % self.columns) { lastColumnIndex in
+                            NavigationLink(destination: ProfileView(profile: self.getProfile(lastColumnIndex: lastColumnIndex))) {
+                                ProfileCell(profile: self.getProfile(lastColumnIndex: lastColumnIndex))
+                                    .frame(width: self.cellWidth(width: geometry.size.width),
+                                           height: self.cellHeight(width: geometry.size.width))
+                                    .border(Color.black, width: 2)
+                                    .clipped()
+                            }
                         }
                         Spacer()
                     }
                 }
             }
         }.padding()
+    }
+
+    private func getProfile(rowIndex: Int, columnIndex: Int) -> Profile {
+        return viewModel.profiles[columns * rowIndex + columnIndex]
+    }
+
+    private func getProfile(lastColumnIndex: Int) -> Profile {
+        return viewModel.profiles[self.columns * (viewModel.profiles.count / columns) + lastColumnIndex]
     }
 
     private func cellWidth(width: CGFloat) -> CGFloat {
