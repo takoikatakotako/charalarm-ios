a751b874ba4bae32bb9aa8507e4c6aa0118fd146
snorlax.chemist.and.jazz@gmail.com <snorlax.chemist.and.jazz@gmail.com>
2020-02-09 04:05:04 +0900

    ダウンロードURLでダウンロードできるようにした。

--- CharacterAlarm/CharacterList/CharacterListView.swift
+++ CharacterAlarm/CharacterList/CharacterListView.swift
@@ -1,21 +1,23 @@
 import SwiftUI
 import UIKit
 import SDWebImageSwiftUI
+import FirebaseStorage
 
 struct ProfileCell: View {
     let profile: Profile
+    @State var urlString: String = ""
     var body: some View {
         ZStack {
-            WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic"))
+            WebImage(url: URL(string: self.urlString))
                 .resizable()
                 .placeholder {
                     Image("profile")
                         .resizable()
             }
-                .animation(.easeInOut(duration: 0.5))
-                .transition(.fade) 
-                .scaledToFill()
-            
+            .animation(.easeInOut(duration: 0.5))
+            .transition(.fade)
+            .scaledToFill()
+
             VStack {
                 Spacer()
                 Text(profile.name)
@@ -25,15 +27,33 @@ struct ProfileCell: View {
                     .foregroundColor(Color.white)
                     .opacity(0.8)
             }
+        }.onAppear {
+            self.featchImageUrl()
+        }
+    }
+
+    func featchImageUrl() {
+        let storage = Storage.storage()
+        let pathReference = storage.reference(withPath: "character/\(profile.id)/profile.png")
+        pathReference.downloadURL { url, error in
+            if let error = error {
+                // Handle any errors
+                print(error)
+            } else {
+                guard let urlString = url?.absoluteString else {
+                    return
+                }
+                self.urlString = urlString
+            }
         }
     }
 }
 
 struct CharacterListView: View {
     @ObservedObject(initialValue: CharacterListViewModel()) var viewModel: CharacterListViewModel
-    
+
     private let columns: Int = 3
-    
+
     var body: some View {
         GeometryReader { geometry in
             ScrollView(showsIndicators: false) {
@@ -47,7 +67,7 @@ struct CharacterListView: View {
                         }
                     }
                 }
-                
+
                 if (self.viewModel.profiles.count % self.columns > 0) {
                     HStack {
                         ForEach(0..<self.viewModel.profiles.count % self.columns) { lastColumnIndex in
@@ -62,23 +82,23 @@ struct CharacterListView: View {
             }
         }.padding()
     }
-    
+
     private func getProfile(rowIndex: Int, columnIndex: Int) -> Profile {
         return viewModel.profiles[columns * rowIndex + columnIndex]
     }
-    
+
     private func getProfile(lastColumnIndex: Int) -> Profile {
         return viewModel.profiles[self.columns * (viewModel.profiles.count / columns) + lastColumnIndex]
     }
-    
+
     private func cellWidth(width: CGFloat) -> CGFloat {
         return width / CGFloat(columns)
     }
-    
+
     private func cellHeight(width: CGFloat) -> CGFloat {
         return cellWidth(width: width) * 1.5
     }
-    
+
     private func getProfileCell(profile: Profile, width: CGFloat, height: CGFloat) -> AnyView {
         return AnyView(NavigationLink(destination: ProfileView(profile: profile)) {
             ProfileCell(profile: profile)
