1ce8ead01effa71f926be7c39703bece0a39968c
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2020-09-08 19:15:59 +0900

    ビルド番号を更新して個人情報を見れるようにした

--- CharacterAlarm/Views/UserInfo/UserInfoView.swift
+++ CharacterAlarm/Views/UserInfo/UserInfoView.swift
@@ -1,11 +1,22 @@
 import SwiftUI
 
 struct UserInfoView: View {
+    
+    var userName: String {
+        guard let anonymousUserName = UserDefaultsStore.getAnonymousUserName() else {
+            return ""
+        }
+        return anonymousUserName
+    }
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 8) {
+            Text("ユーザー情報")
+                .font(Font.body.bold())
+            Text(userName)
+
             Spacer()
         }
+        .padding()
     }
 }
 
