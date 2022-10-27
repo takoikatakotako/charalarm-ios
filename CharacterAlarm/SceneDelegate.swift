01bf377a0158f7f601be3ed78a0d78442fdb57f3
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2022-10-27 16:21:14 +0900

    AppDelegate, SceneDelegateのリファクタリング

--- CharacterAlarm/SceneDelegate.swift
+++ CharacterAlarm/SceneDelegate.swift
@@ -2,43 +2,22 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
     var rootViewController: UIViewController?
+    let sceneDelegateModel = SceneDelegateModel()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         // キャラクターの初期値のDomainを登録
-        charalarmEnvironment.userDefaultsHandler.registerDefaults(defaults: [CHARA_DOMAIN : DEFAULT_CHARA_DOMAIN, CHARA_NAME: DEFAULT_CHARA_NAME])
+        sceneDelegateModel.registerDefaults()
         
-        // 
-        guard let charaDomain = charalarmEnvironment.userDefaultsHandler.getCharaDomain()  else {
-            fatalError("キャラクターのドメインの取得に失敗しました")
-        }
-        if let data = try? charalarmEnvironment.fileHandler.loadData(directoryName: charaDomain, fileName: "resource.json"),
-           let resource: Resource = try? JSONDecoder().decode(Resource.self, from: data){
-            print("ResourceVersion: \(resource.version)")
-            // TODO: 選択中のキャラクターの最新リソースがある場合更新する
-        } else {
-            // データをロード
-            if let fileUrl = Bundle.main.url(forResource: "resource", withExtension: "json", subdirectory: "Resource/com.charalarm.yui"),
-               let data = try? Data(contentsOf: fileUrl),
-               let resource: Resource = try? JSONDecoder().decode(Resource.self, from: data) {
-                loadData(charaDomain: charaDomain, resource: resource)
-            }
-        }
+        // データをロードする
+        sceneDelegateModel.loadData()
+
+        // チュートリアルが終わったのか判定
+        let doneTutorial = sceneDelegateModel.getIsDoneTutorial()
+        let appVersion = sceneDelegateModel.getAppVersion()
         
-        //
-        let anonymousUserName = charalarmEnvironment.keychainHandler.getAnonymousUserName()
-        let anonymousUserPassword = charalarmEnvironment.keychainHandler.getAnonymousAuthToken()
-        
-        // 匿名ユーザー名、パスワードが登録されていればチュートリアル完了
-        let doneTutorial = anonymousUserName != nil && anonymousUserPassword != nil
-        
-        guard let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
-            fatalError("アプリケーションのバージョンの取得に失敗しました。")
-        }
-        
+        // RootViewを作成
         let rootView = RootView()
         let charalarmAppState = CharalarmAppState(appVersion: appVersion)
         charalarmAppState.doneTutorial = doneTutorial
@@ -51,47 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.makeKeyAndVisible()
         }
     }
-    
-    func loadData(charaDomain:String, resource: Resource) {
-        do {
-            let encoder = JSONEncoder()
-            let data = try encoder.encode(resource)
-            try charalarmEnvironment.fileHandler.saveFile(directoryName: charaDomain, fileName: "resource.json", data: data)
-        } catch {
-            return
-        }
         
-        for imageName in resource.resource.images {
-            let fileInfo = imageName.split(separator: ".")
-            do {
-                let fileName: String = String(fileInfo[0])
-                let type: String = String(fileInfo[1])
-                guard let fileUrl = Bundle.main.url(forResource: fileName, withExtension: type, subdirectory: "Resource/com.charalarm.yui/image"),
-                      let data = try? Data(contentsOf: fileUrl) else {
-                    continue
-                }
-                try charalarmEnvironment.fileHandler.saveFile(directoryName: charaDomain, fileName: imageName, data: data)
-            } catch {
-                print("画像ファイルの書き込みに失敗")
-            }
-        }
-        
-        for voiceName in resource.resource.voices {
-            let fileInfo = voiceName.split(separator: ".")
-            do {
-                let fileName: String = String(fileInfo[0])
-                let type: String = String(fileInfo[1])
-                guard let fileUrl = Bundle.main.url(forResource: fileName, withExtension: type, subdirectory: "Resource/com.charalarm.yui/voice"),
-                      let data = try? Data(contentsOf: fileUrl) else {
-                    continue
-                }
-                try charalarmEnvironment.fileHandler.saveFile(directoryName: charaDomain, fileName: voiceName, data: data)
-            } catch {
-                print("ボイスファイルの書き込みに失敗")
-            }
-        }
-    }
-    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
