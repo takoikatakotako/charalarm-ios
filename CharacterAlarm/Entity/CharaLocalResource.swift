b16d4059d0d5132defbf1e02ef7178e39a0ae43a
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-06-09 23:38:52 +0900

    Extensionを修正

--- CharacterAlarm/Entity/CharaLocalResource.swift
+++ CharacterAlarm/Entity/CharaLocalResource.swift
@@ -2,13 +2,28 @@ import Foundation
 
 struct CharaLocalResource {
     let charaID: String
-    let expressions: [String: CharaLocalExpression]
+    var expressions: [String: CharaLocalExpression]
+    
+    init(chara: Chara) {
+        self.charaID = chara.charaID
+
+        var expressions: [String: CharaLocalExpression] = [:]
+        chara.expressions.forEach { touple in
+            expressions[touple.key] = CharaLocalExpression(charaExpression: touple.value)
+        }
+        self.expressions = expressions
+    }
 }
 
 
 struct CharaLocalExpression: Hashable, Equatable {
     let imageFileNames: [String]
     let voiceFileNames: [String]
+    
+    init(charaExpression: CharaExpression) {
+        self.imageFileNames = charaExpression.images
+        self.voiceFileNames = charaExpression.voices
+    }
 }
 
 
